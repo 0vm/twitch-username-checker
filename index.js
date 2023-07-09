0edc83d2fbb18d32938153281389907767b26a19
Ak <79897291+0vm@users.noreply.github.com>
2023-07-09 11:36:12 +1000

    better error handling

--- index.js
+++ index.js
@@ -2,76 +2,83 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 function* generateUsernames() {
-    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-    for (let i = 0; i < alphabet.length; i++) {
-        for (let j = 0; j < alphabet.length; j++) {
-            for (let k = 0; k < alphabet.length; k++) {
-                for (let l = 0; l < alphabet.length; l++) {
-                    yield alphabet[i] + alphabet[j] + alphabet[k] + alphabet[l];
-                }
-            }
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  for (let i = 0; i < alphabet.length; i++) {
+    for (let j = 0; j < alphabet.length; j++) {
+      for (let k = 0; k < alphabet.length; k++) {
+        for (let l = 0; l < alphabet.length; l++) {
+          yield alphabet[i] + alphabet[j] + alphabet[k] + alphabet[l];
         }
+      }
     }
+  }
 }
 
 const usernames = Array.from(generateUsernames());
 
 async function checkUsername(browser, username) {
-    const page = await browser.newPage();
-    // Set user agent
-    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0');
+  const page = await browser.newPage();
+  // Set user agent
+  await page.setUserAgent(
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0'
+  );
 
-    // disable images, CSS and fonts from loading
-    await page.setRequestInterception(true);
-    page.on('request', (request) => {
-        if (['image', 'stylesheet', 'font'].includes(request.resourceType())) {
-            request.abort();
-        } else {
-            request.continue();
-        }
-    });
-
-    try {
-        await page.goto(`https://www.twitch.tv/${username}`);
-        await page.waitForTimeout(4000); // wait for 4s
-        const bodyHTML = await page.evaluate(() => document.body.innerHTML);
-        if (bodyHTML.includes('Sorry. Unless you’ve got a time machine, that content is unavailable.')) {
-            fs.appendFileSync('availableUsernames.txt', `${username} \n`);
-        }
-    } catch (error) {
-        console.error(`An error occurred while checking user ${username}:`, error);
-    } finally {
-        await page.setContent('');
-        await page.close();
+  // disable images, CSS and fonts from loading
+  await page.setRequestInterception(true);
+  page.on('request', (request) => {
+    if (['image', 'stylesheet', 'font'].includes(request.resourceType())) {
+      request.abort();
+    } else {
+      request.continue();
     }
+  });
+
+  try {
+    await page.goto(`https://www.twitch.tv/${username}`);
+    await page.waitForTimeout(4000); // wait for 4s
+    const bodyHTML = await page.evaluate(() => document.body.innerHTML);
+    if (bodyHTML.includes('Sorry. Unless you’ve got a time machine, that content is unavailable.')) {
+      fs.appendFileSync('availableUsernames.txt', `${username} \n`);
+    }
+  } catch (error) {
+    console.error(`An error occurred while checking user ${username}:`, error);
+  } finally {
+    await page.setContent('');
+    await page.close();
+  }
 }
 
 async function checkUsernames() {
-    const browser = await puppeteer.launch({
-        headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    });
-    const parallelBrowsingCount = 20;
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
+  const parallelBrowsingCount = 20;
 
-    let startIndex = 0;
+  let startIndex = 0;
+  try {
+    const availableUsernames = fs.readFileSync('available.txt', 'utf8').split('\n').filter(Boolean);
+    startIndex = usernames.findIndex(username => !availableUsernames.includes(username));
+  } catch (error) {
+    console.error('An error occurred while reading available usernames:', error);
+    return; // Stop execution if an error occurs
+  }
+
+  for (let i = startIndex; i < usernames.length; i += parallelBrowsingCount) {
+    let promises = [];
+    for (let j = 0; j < parallelBrowsingCount; j++) {
+      if (usernames[i + j]) {
+        promises.push(checkUsername(browser, usernames[i + j]));
+      }
+    }
     try {
-        const availableUsernames = fs.readFileSync('available.txt', 'utf8').split('\n').filter(Boolean);
-        startIndex = usernames.findIndex(username => !availableUsernames.includes(username));
+      await Promise.all(promises);
     } catch (error) {
-        console.error('An error occurred while reading available usernames:', error);
+      console.error('An error occurred while checking usernames:', error);
     }
+  }
 
-    for (let i = startIndex; i < usernames.length; i += parallelBrowsingCount) {
-        let promises = [];
-        for (let j = 0; j < parallelBrowsingCount; j++) {
-            if (usernames[i + j]) {
-                promises.push(checkUsername(browser, usernames[i + j]));
-            }
-        }
-        await Promise.all(promises);
-    }
-
-    await browser.close();
+  await browser.close();
 }
 
 checkUsernames();
